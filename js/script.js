d028960d7c5c3375524245fbfbd01f0ae11cc237
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-08-06 15:32:05 +0200

    html + css + js start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// ISTRUZIONI PER L'UTENTE
+alert("Scrivi il tuo nome, il pc ti salutera' scrivendo il nome cambiandolo di colore a seconda del sesso. ")
+
+// INSERIRE IL NOME UTENTE
+
+// INSERIRE IL SESSO UTENTE
+
+// CONTROLLO INSERIMENTO DATI
+
+// VERIFICA DEL SESSO PER IL CAMBIO COLORE
